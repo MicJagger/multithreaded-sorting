f9ae2f64fafbbdf59a1bb6c848f817ca1b46fa7b
Michael Jagiello <michaeljr623@hotmail.com>
2024-08-18 23:19:19 -0500

    Updated Readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# sorting
-Testing Sorting Algorithms and Multithreading them  \
-Currently built for visual studio on windows, will change in the future
+# Sorting Algorithms
+While this repository focuses on sorting algorithms and their performance, it goes a bit more into detail
+
+For example, it focuses on multithreading different algorithms to see how well they scale across multiple hardware threads
+
---
+++ original-deprecated/sorting.h
@@ -0,0 +1,62 @@
+#pragma once
+#ifndef _SORTING_H
+#define _SORTING_H
+
+#include <chrono>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <queue>
+#include <random>
+#include <string>
+#include <thread>
+#include <vector>
+
+typedef void(functionPtr)(int*, int);
+
+namespace MJsort {
+	// random list
+	void RandomList(int* array, int size); // 15-bit integers
+	void RandomList30(int* array, int size); // 30-bit integers
+	void RandomList32(int* array, int size); // 32-bit integers
+	// meant to be "as random as possible"
+	
+	// copy array
+	void CopyArray(int* array1, int* array2, int size); // copy array1 to array2
+
+	// test validity
+	bool TestValid(int* array, int* correctArray, int size);
+
+	// setup
+	double MainSetup(int* unsorted, int* correct, int size);
+	void Setup(std::string* name, int* unsorted, int* toSort, int size, int i, int j, bool first, std::vector<std::string>* names);
+
+	// helper algorithms
+	namespace helper {
+
+	}
+
+	// algorithms
+	void StandardSort(int* array, int size);
+	void BubbleSort(int* array, int size);
+
+	// Timed
+	double SortTimed(int* array, int size, functionPtr* func);
+
+	// Multi-Threaded w/ Full array
+	void Combine2ArrayA(int* array1, int* array2, int size1, int size2);
+	void ThreadedSortA(int* array, int size, int threads, functionPtr* func);
+	double ThreadedSortATimed(int* array, int size, int threads, functionPtr* func);
+
+	// Multi-Threaded w/ Queue
+	void Combine2ArrayQ(int* array1, int* array2, int size1, int size2);
+	void ThreadedSortQ(int* array, int size, int threads, functionPtr* func);
+	double ThreadedSortQTimed(int* array, int size, int threads, functionPtr* func);
+
+	// Mult-Threaded w/ Swaps
+	//double ThreadedSortSwaps(int* array, int size);
+}
+
+// C:\All Other Data\School\5 Spring 2024\Data Structures and Algorithm\Project 3\DSA Project 3
+
+#endif 
